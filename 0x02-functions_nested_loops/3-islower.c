46ba0f4ca5e6a39c157a20df0b86bf0064929bf4
musolinii <67822077+musolinii@users.noreply.github.com>
2023-02-21 21:12:00 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ * @c: caracter recibo por main.
+ * Return: 1 if c is lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
