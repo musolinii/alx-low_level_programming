e7c09de94073f76018cde456ee19bed0aa8d6fed
musolinii <67822077+musolinii@users.noreply.github.com>
2023-02-22 20:51:41 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Suma dos enteros por función.
+ * @i: Primera varable entera a sumar.
+ * @l: Segunda variable a sumar.
+ * Return: retorno con sumatoria
+ */
+int add(int a , int b)
+{
+	int n;
+	 n = a + b;
+	return (n);
+}
