cf626cbe70622698fff55b828b9e2bba20bee484
musolinii <67822077+musolinii@users.noreply.github.com>
2023-02-24 10:31:06 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for lowercase character.
+ * @c: caracter recibo por main.
+ * Return: 1 if c is uppercase
+ */
+int mul(int a, int b)
+{
+  int product
+  product=a*b;
+  
+  return(product)
+ }
