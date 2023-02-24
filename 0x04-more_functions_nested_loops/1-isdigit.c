ca4793229e7789c7668ecf2ab6e2ca1beb758792
musolinii <67822077+musolinii@users.noreply.github.com>
2023-02-24 10:22:03 +0300

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for lowercase character.
+ * @c: caracter recibo por main.
+ * Return: 1 if c is uppercase
+ */
+int _isdigit(int c)
+{
+	if (c >= '1' && c <= '0')
+	{
+		return (1);
+	}
+  
+	else
+	{
+		return (0);
+	}
+}
