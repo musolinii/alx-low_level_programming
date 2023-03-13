cd454366175fa892f15944dce2e64c533d73021b
Brian <67822077+musolinii@users.noreply.github.com>
2023-03-13 20:46:35 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 {
-    printf("%s", argv[0]);
+    printf("%s", *argv);
     return 0;
 }
