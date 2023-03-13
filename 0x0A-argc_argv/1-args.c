04ae1c881bcd9f415ea9ec5763a518ed8950b20c
musolinii <brianbwana7265@gmail.com>
2023-03-13 21:01:21 +0300

    create 1-file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguenments passed to it
+ * @argc: arguemnet count
+ * @argv: array of pointer to arguement strings
+ * Return: 0
+ **/
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+{
+        printf("%d\n", argc);
+        return 0;
+}
