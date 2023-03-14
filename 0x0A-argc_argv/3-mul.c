21776bbb3edaa65d231b24598178da155f89e714
Brian <67822077+musolinii@users.noreply.github.com>
2023-03-14 19:29:17 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,11 @@
 #include<stdlib.h>
 #include<stdio.h>
-
+/**
+ * main - multiply two numbers
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: result of multiplication or 1
+ **/
 int main(int argc, char *argv[])
 {
         int product = 0;
@@ -11,7 +16,7 @@ int main(int argc, char *argv[])
                 printf("Error\n");
                 return 1;
         }
-
+/* atoi convert a string to an int */
         a = atoi(argv[1]);
         b = atoi(argv[2]);
         product = a * b;
