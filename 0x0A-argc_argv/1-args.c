39b70d73976c72215e239f9447da55b068711930
Brian <67822077+musolinii@users.noreply.github.com>
2023-03-15 19:52:24 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,10 @@
  * @argv: array of pointer to arguement strings
  * Return: 0
  **/
-int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
-        printf("%d\n", argc - 1);
-        return 0;
+/* to suppress unused variable warning */
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
 }
